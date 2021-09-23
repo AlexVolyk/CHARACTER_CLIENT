448ccffcc69978f6a1f280324aa1eb11bbf6cda5
AlexVolyk <85078173+AlexVolyk@users.noreply.github.com>
2021-09-23 20:34:37 +0300

    comment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'https://characterclient.herokuapp.com':
+    case 'characterclient.herokuapp.com':
         APIURL = 'https://characterserver.herokuapp.com'
         break;
 }
