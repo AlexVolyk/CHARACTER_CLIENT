879040b0dbfd0d259543b9f25f35c5d258a94ce0
AlexVolyk <85078173+AlexVolyk@users.noreply.github.com>
2021-09-26 22:04:47 +0300

    work

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,13 +5,10 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-
     case 'freya-alex-kinsey-client.herokuapp.com':
-        APIURL = 'freya-alex-kinsey-server.herokuapp.com'
+        APIURL = 'https://freya-alex-kinsey-server.herokuapp.com';
         break;
 }
-    // case 'characterclient.herokuapp.com':
-    //     APIURL = 'https://characterserver.herokuapp.com'
-    //     break;
+
 
 export default APIURL;
